c0be647deb154dc329c1f05b58ef0988a984a776
AyKrimino <ayoub.krimi14@gmail.com>
2025-11-22 21:18:40 +0100

    refactor: use bufio.Scanner for proper json message framing

--- internal/gossip/gossip.go
+++ internal/gossip/gossip.go
@@ -1,6 +1,7 @@
 package gossip
 
 import (
+	"bufio"
 	"encoding/json"
 	"log"
 	"net"
@@ -55,74 +56,72 @@ func (g *Gossip) handleConn(conn net.Conn) {
 	defer conn.Close()
 	log.Printf("[GOSSIP] New peer connection from %s", conn.RemoteAddr())
 
-	// Receive JOIN message
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Printf("[GOSSIP] Read error: %s", err)
-	}
-	log.Printf("[GOSSIP] we've read %s", buf[:n])
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		var joinMsg JoinMessage
+		if err := json.Unmarshal(line, &joinMsg); err != nil {
+			log.Printf("[GOSSIP] Unmarshal error: %s", err)
+			continue
+		}
 
-	var joinMsg JoinMessage
-	if err = json.Unmarshal(buf[:n], &joinMsg); err != nil {
-		log.Printf("[GOSSIP] Unmarshal error: %s", err)
-	}
-	log.Printf("[GOSSIP] %+v received from %s", joinMsg, conn.RemoteAddr().String())
+		log.Printf("[GOSSIP] %+v received from %s", joinMsg, conn.RemoteAddr().String())
 
-	// Update peers list
-	g.updatePeersList(joinMsg.Sender)
+		g.updatePeersList(joinMsg.Sender)
 
-	// send JOIN_RESPONSE message
-	joinResMsg := NewJoinResponseMessage(g.peers)
-	joinResMsgB, err := json.Marshal(joinResMsg)
-	if err != nil {
-		log.Printf("[GOSSIP] Marshal error: %s", err)
+		joinResMsg := NewJoinResponseMessage(g.peers)
+		joinResMsgB, err := json.Marshal(joinResMsg)
+		if err != nil {
+			log.Printf("[GOSSIP] Marshal error: %s", err)
+		}
+		joinResMsgB = append(joinResMsgB, '\n')
+		n, err := conn.Write(joinResMsgB)
+		if err != nil {
+			log.Printf("[GOSSIP] conn write error: %s", err)
+		}
+		log.Printf("[GOSSIP] %s sent from %s to %s", joinResMsgB[:n], g.addr, conn.RemoteAddr().String())
 	}
-
-	n, err = conn.Write(joinResMsgB)
-	if err != nil {
-		log.Printf("[GOSSIP] conn write error: %s", err)
+	if err := scanner.Err(); err != nil {
+		log.Printf("[GOSSIP] Scanner error: %s", err)
 	}
-	log.Printf("[GOSSIP] %s sent from %s to %s", joinResMsgB[:n], g.addr, conn.RemoteAddr().String())
 }
 
-// Join dials bootstrap
 func (g *Gossip) Join(bootstrapAddr string) {
 	conn, err := net.Dial("tcp", bootstrapAddr)
 	if err != nil {
 		log.Printf("[GOSSIP] TCP dial error: %s", err)
 	}
 
-	// send JOIN message
 	joinMsg := NewJoinMessage(g.addr)
 	joinMsgB, err := json.Marshal(joinMsg)
 	if err != nil {
 		log.Printf("[GOSSIP] Marshal error: %s", err)
+		return
 	}
 
+	joinMsgB = append(joinMsgB, '\n')
 	n, err := conn.Write(joinMsgB)
 	if err != nil {
 		log.Printf("[GOSSIP] conn write error: %s", err)
+		return
 	}
-
 	log.Printf("[GOSSIP] %s sent from %s to %s", joinMsgB[:n], conn.LocalAddr().String(), bootstrapAddr)
 
-	// Receive JOIN_RESPONSE message
-	buf := make([]byte, 1024)
-	n, err = conn.Read(buf)
-	if err != nil {
-		log.Printf("[GOSSIP] Read error: %s", err)
-	}
-	log.Printf("[GOSSIP] we've read %s", buf[:n])
+	scanner := bufio.NewScanner(conn)
+	if scanner.Scan() {
+		line := scanner.Bytes()
+		var joinResMsg JoinResponseMessage
+		if err := json.Unmarshal(line, &joinResMsg); err != nil {
+			log.Printf("[GOSSIP] Unmarshal error: %s", err)
+			return
+		}
+		log.Printf("[GOSSIP] %+v received from %s", joinResMsg, bootstrapAddr)
 
-	var joinResMsg JoinResponseMessage
-	if err = json.Unmarshal(buf[:n], &joinResMsg); err != nil {
-		log.Printf("[GOSSIP] Unmarshal error: %s", err)
+		g.updatePeersList(joinResMsg.Peers...)
 	}
-	log.Printf("[GOSSIP] %+v received from %s", joinResMsg, conn.RemoteAddr().String())
-
-	// Update peers list
-	g.updatePeersList(joinResMsg.Peers...)
+	if err := scanner.Err(); err != nil {
+		log.Printf("[GOSSIP] Scanner error: %s", err)
+	}
 }
 
 func (g *Gossip) updatePeersList(newPeers ...string) {
